d1038e0ce61cecd912f9786e794f52845c058a3b
Jin562023 <147892456+Jin562023@users.noreply.github.com>
2023-11-04 06:26:20 +0000

    8-3

--- tools.py
+++ tools.py
@@ -9,22 +9,23 @@ def playGame():
     print("============猜數字遊戲=================\n")
     while(True):
         keyin = int(input(f"猜數字的範圍{min}~{max}"))
-            count += 1
-            if(keyin >= min and keyin <= max):
-                if keyin == target:
+        count += 1
+        if(keyin >= min and keyin <= max):
+             if keyin == target:
                     print(f"賓果!猜對了, 答案是:{keyin}")
                     break
-                elif keyin > target:
+             elif keyin > target:
                     print("再小一點")
                     max = keyin - 1
-                elif keyin < target:
+             elif keyin < target:
                     print("再大一點")
                     min = keyin + 1
-                print(f"加油@您已經猜了{count}次")
-                continue
-            else:
-                print("請輸入提示範圍內的數字")
-        print(f"您猜了{count}次")
+             print(f"加油@您已經猜了{count}次")
+             continue
+        else:
+            print("請輸入提示範圍內的數字")
+    print(f"您猜了{count}次")
+          
 
 
 class Student():
@@ -35,4 +36,7 @@ class Student():
         self.math = match
 
     def sum(self)->int: #實體方法
-        return self.chinese + self.english + self.math       
+        return self.chinese + self.english + self.math  
+
+def getStudent(name:str,chinese:int,math:int,english:int) -> Student:
+    return Student(name=name,chinese=chinese,english=english,math=math)     
